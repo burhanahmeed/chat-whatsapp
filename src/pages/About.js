2128e47bf2bd688c4d32f414a87a9593ee634197
burhan ahmed <burhanahmeed@gmail.com>
2020-01-25 18:03:28 +0700

    twtitter card

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,44 @@
 import React from 'react';
-import { Link } from 'react-router';
+import ReactMarkdown from 'react-markdown';
+
+import '../style/markdown.css';
 
 export default class About extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+            markdown: ''
+        }
+    }
+
+    componentWillMount () {
+        let self = this;
+        var url = `https://api.github.com/repos/burhanahmeed/reactjs-whatsapp-redirector/contents/README.md`;
+        var myRequest = new Request(url, { headers: new Headers({'accept':'application/vnd.github.v3.raw'})});
+        fetch(myRequest).then(function(response) {
+            if (!response.ok) return "# There was error with your response, please check the details and try again";
+                return response.text();
+            }
+        ).then(function(response) {
+            let md = response;
+            self.setState({
+                markdown: md
+            })
+        });
+    }
+
     render () {
         return (
-            <div>
-                <div>
+            <div className="wrapper">
+                <div className="about">
                     <a href="https://github.com/burhanahmeed/reactjs-whatsapp-redirector" target="_blank">
                         Repository to contribute
                     </a>
+                    <div className="content-md">
+                        <ReactMarkdown
+                            source={this.state.markdown}
+                        ></ReactMarkdown>
+                    </div>
                 </div>
             </div>
         );
