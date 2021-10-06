bbdc917b5d97835359225766977cb23efc1862e0
boidushya <boidushyabhattacharya@gmail.com>
2021-10-06 13:52:57 +0530

    feat(PWA): Configured PWA and added service workers

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import { ThemeProvider, CSSReset } from "@chakra-ui/core"; 
 import customTheme from "./theme.js";
@@ -10,11 +10,12 @@ import customTheme from "./theme.js";
 ReactDOM.render(
   <ThemeProvider theme={customTheme}>
     <CSSReset />
-    <App />        
+    <App />
   </ThemeProvider>
 , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorkerRegistration.register();
+
